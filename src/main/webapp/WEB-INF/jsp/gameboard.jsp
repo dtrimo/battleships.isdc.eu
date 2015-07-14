a6f8ea165dbee7c98316be9271e9ecdb1a3f36e2
cuvidk <cuvidk@yahoo.ro>
2015-07-14 12:53:26 +0300

    Script files reorganized Starts to reorganize scripts, splits the gameBoard.js(gameSelectionScript.js currently) in more relevant, organized, and oop scripts. TO DO: transformBoard, gameBoard scripts, overall reorganization.

--- src/main/webapp/WEB-INF/jsp/gameboard.jsp
+++ src/main/webapp/WEB-INF/jsp/gameboard.jsp
@@ -5,7 +5,9 @@
 	<script type="text/javascript" src="js/jquery-1.11.3.js"></script>
 	<script type="text/javascript" src="js/jquery-ui.js"></script>
 	<link rel="stylesheet" type="text/css" href="css/gameboard.css">
-	<script src="js/gameboard.js" type="text/javascript"></script>	
+	<script src="js/board.js" type="text/javascript"></script>
+	<script src="js/selectionBoard.js" type="text/javascript"></script>
+	<script src="js/gameSelectionScript.js" type="text/javascript"></script>	
 	<style>
 		.active {
 			background-color: grey;
--- src/main/webapp/js/gameSelectionScript.js
+++ src/main/webapp/js/gameSelectionScript.js
@@ -2,108 +2,7 @@ var generateBoard = function(){
 	var mGameBoard;
 	var nGameBoard;
 	var $container;
-
-	var init = function(lines,columns,tableContainer){
-		nGameBoard = lines;
-		mGameBoard = columns;
-		$container = $(tableContainer); 
-	}
-
-	var drawBoard = function(){
-		for(var i = 0; i < nGameBoard ; i++){
-			var $divRow = $('<div><div>');
-			$divRow.addClass('div-row');
-			for(var j = 0; j < mGameBoard; j++){
-				var $div = $('<div></div>');
-				$divRow.append($div);
-			}
-			$container.append($divRow);
-		}
-
-	}
  	
- 	//board has n lines and m columns
-	var drawShipsBoard = function(m, n){
-		var $shipsContainer = $('.ships-container');
-
-		for(var i = 0; i < n; i++){
-			var $divRow = $('<div><div>');
-			$divRow.addClass('div-row');
-			for(var j = 0; j < m; j++){
-				var $div = $('<div></div>');
-				$divRow.append($div);
-			}
-			$shipsContainer.append($divRow);
-		}
-		$shipsContainer.data( "size", { lines: n, columns: m });
-	}
-
-	//draw ships in shipsContainer using arrays with coordinates x,y; ship starts at offset
-	//board has n lines and m columns
-	//offsetUp - distance from top to ship(in cells -> .ship-cell)
-	//offset - distance from left to ship(in cells -> .ship-cell)
-	var drawShip = function(x, y, offsetX, offsetY, shipsContainer){
-
-		var maxX = Math.max.apply(null, x);
-		var minX = Math.min.apply(null, x); 
-		var maxY = Math.max.apply(null, y);
-		var minY = Math.min.apply(null, y); 
-		
-		var shipCellWidth = maxX-minX+1;
-		var shipCellHeight = maxY-minY+1;
-		
-		var $divShip = $('<div></div>');
-		$divShip.addClass('div-ship');
-		
-		if (shipsContainer.is('.transformation-container')){
-			$divShip.addClass('transformShip');
-		} else {
-			$divShip.addClass('modelShip');
-		}
-		
-		
-		var $div = $($(shipsContainer).find('.div-row')[0]);
-		var $cell = $($div.find(' > div')[0]);
-		var cellHeight = $cell.outerHeight();
-		var cellWidth = $cell.outerWidth();
-
-		$divShip.outerWidth(cellWidth * shipCellWidth);
-		$divShip.outerHeight(cellHeight * shipCellHeight);
-		if ($(shipsContainer).is(".transformation-container")){
-			$(shipsContainer).find(".transformation-grid").append($divShip);		
-		} else {
-			$(shipsContainer).append($divShip);			
-		}
-
-
-		for(var i = 0;i<shipCellHeight;i++){
-			var $divRow = $('<div class="ship-row"></div>');
-			for(var j = 0; j < shipCellWidth; j++){
-				var $div = $('<div class="ship-cell"></div>');
-				$div.outerWidth(cellWidth);
-				$div.outerHeight(cellHeight);
-				$divRow.append($div);
-			}
-			$divShip.append($divRow);
-		}
-
-		var left = (offsetX+minX)*cellWidth;
-		$divShip.css('left',left + 'px');
-
-		for(var i = 0; i < y.length; i++){
-			var $div = $($divShip.find('.ship-row')[shipCellHeight-y[i]+minY-1]);
-			var $cell = $($div.find(' > div')[x[i]-minX]);
-			$cell.css('background-color','black');
-		}
-
-		var bottom = (offsetY+minY)*cellHeight;
-		$divShip.css('bottom', bottom + 'px');
-
-		$divShip.data("position",BattleshipPosition(x,y));
-		
-		return shipCellWidth+1;
-	}
-
 	//draws transfomations container whit m x n cells
 	var drawTransformationContainer = function(m, n){
 		var $container = $('.transformation-container');
@@ -126,10 +25,6 @@ var generateBoard = function(){
 
 
 	return{
-		init : init,
-		drawBoard : drawBoard,
-		drawShipsBoard : drawShipsBoard,
-		drawShip: drawShip,
 		drawTransformationContainer : drawTransformationContainer
 	}
 };
@@ -292,52 +187,51 @@ var gameEditing = function(data){
 	 
 	var $selectedShip = null;
 	var $draggingShip = null;
- 	var board = generateBoard();
- 	var $container =  $($('.board-container')[0])
-	var $shipsContainer = $('.ships-container');
- 	var $transformationContainer = $('.transformation-container');
-
- 	
- 	
- 	
- 	
- 	//Send to server Game type information from the users
- 	//Get From Server response
- 	
- 	board.init(9, 9, $container);
- 	board.drawBoard();
- 	board.drawShipsBoard(23, 4);
 	
+	var $container =  $($('.board-container')[0]);
+ 	var board = new Board(9,9,$container);
+ 	board.drawBoard();
+ 		
+ 	var $transformationContainer = $('.transformation-container');
+ 	var transformationBoard = new Board(14,14, $transformationContainer, "transformShip");
+ 	transformationBoard.drawBoard();
  	
+	var $shipsContainer = $('.ships-container');
+	selectionBoard = new SelectionBoard(23, 4, $shipsContainer, "modelShip", transformationBoard);
+	selectionBoard.drawBoard();
+	
+	$(document).on("selectedShipChanged",function(event,data){
+		$selectedShip = data;
+	});
+
  	/*	Draw ships in ships-container - June 25. 2015. added idIndex, so 
  	 * 	redraw function could be easier. Supposed BUGS could be available   */
 	var offset = 0;
 	
  	var x = [0,1,2,1];
 	var y = [0,0,0,1];
- 	offset += board.drawShip(x, y, offset, 0, $shipsContainer);
+ 	offset += selectionBoard.drawShip(x, y, offset, 0, $shipsContainer).width+1;
 
  	var x1 = [0,1,2,1,1];
  	var y1 = [1,0,1,1,2];
- 	offset += board.drawShip(x1, y1, offset, 0, $shipsContainer);
+ 	offset += selectionBoard.drawShip(x1, y1, offset, 0, $shipsContainer).width+1;
  
  	var x2 = [0,0,0,1];
  	var y2 = [0,1,2,0];
- 	offset += board.drawShip(x2, y2, offset, 0, $shipsContainer);
+ 	offset += selectionBoard.drawShip(x2, y2, offset, 0, $shipsContainer).width+1;
 
  	var x3 = [0,1,2];
  	var y3 = [0,0,0];
- 	offset += board.drawShip(x3, y3, offset, 0, $shipsContainer);
+ 	offset += selectionBoard.drawShip(x3, y3, offset, 0, $shipsContainer).width+1;
 
  	var x4 = [0,1,2,3,4];
  	var y4 = [0,0,0,0,0];
- 	offset += board.drawShip(x4, y4, offset, 0, $shipsContainer);
+ 	offset += selectionBoard.drawShip(x4, y4, offset, 0, $shipsContainer).width+1;
 
- 	board.drawTransformationContainer(14, 14);
- 	
+
 	
  	//Dragging
- 	
+	
  	$('.transformation-container, .board-container').on('mouseover',function(){
 
  		$('.transformShip, .gameBoardShip').draggable({
@@ -374,14 +268,7 @@ var gameEditing = function(data){
  	
  	
  	/////////////////////////////////06.23.2015 - Handlers/////////////////////////////////////////////////////////
- 	$('.modelShip').click(function(){ 		
- 		$('div.transformShip').remove();
- 		var $ship = $(this);	
- 		var xCoords = $ship.data("position").getXCoords();
- 		var yCoords = $ship.data("position").getYCoords();
- 	 	board.drawShip(xCoords, yCoords, 6, 6, $transformationContainer);
- 		$selectedShip = $(this);
-	});
+
  	
  	// Needs to be edited: store commands to vector(s).
  	var applyTransformation = function(transformationName){
@@ -442,8 +329,8 @@ var gameEditing = function(data){
 				var x = d2.offset();
 				var rel = { left: y.left-x.left, top: y.top-x.top };
 				
-				var topPosition = dropPosition.top - rel.top;
-				var leftPosition = dropPosition.left - rel.left;
+				topPosition = dropPosition.top - rel.top;
+				leftPosition = dropPosition.left - rel.left;
 				
 			}
 			
@@ -476,7 +363,7 @@ var gameEditing = function(data){
     			}	
 			}
 		}
- 	})
+ 	});
 }
  
 	// Stuff to do:
