9fca12065b519bedb17232ecc6c8d7f67a6d19b4
cuvidk <cuvidk@yahoo.ro>
2015-07-16 10:52:04 +0300

    Transform board middle axis -implements a method for TransformationBoard object that draws the x and y axis on the board

--- src/main/webapp/js/transformationBoard.js
+++ src/main/webapp/js/transformationBoard.js
@@ -11,12 +11,32 @@
 
 	TransformationBoard.prototype.drawBoard = function() {
 		this.board.drawBoard();
+		this.drawAxis();
 	}
 
 	TransformationBoard.prototype.drawShip = function(x, y) {
 		var ship = this.board.drawShip(x, y, this.m / 2, this.n / 2);
 		this.ship = ship;
 		return ship;
+	}
+	
+	TransformationBoard.prototype.drawAxis = function() {
+		var transformBoard = this;
+		var rows = [];
+		rows = this.$container.find($('.div-row'));
+		var numRows = rows.length;
+		for (var i = 0; i < numRows; i++) {
+			$(rows[i]).find('div').each(function(index) {
+				if (i + 1 == transformBoard.n / 2)
+					$(this).css('border-bottom', '2px solid orange');
+				if (i + 1 == transformBoard.n / 2 + 1)
+					$(this).css('border-top', '2px solid orange');
+				if (index + 1 == transformBoard.m / 2)
+					$(this).css('border-right', '2px solid orange');
+				if (index + 1 == transformBoard.m / 2 + 1)
+					$(this).css('border-left', '2px solid orange');
+			});
+		}
 	}
 
 	TransformationBoard.prototype.getShip = function() {
