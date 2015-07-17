f504d22cf6d9f348ff01dd962648ed39716faa6c
cuvidk <cuvidk@yahoo.ro>
2015-07-17 11:57:02 +0300

    Project clean-up -deletes all the useless stuff that's not needed anymore

--- src/main/webapp/js/gameSelectionScript.js
+++ src/main/webapp/js/gameSelectionScript.js
@@ -1,115 +1,21 @@
-//var Transforms = {		
-//		rotateCounterClockwise : function(position){
-//			return {
-//				x: -position.y,
-//				y: position.x
-//			}
-//		},		
-//		rotateClockwise : function(position){
-//			return {
-//				x: position.y,
-//				y: -position.x
-//			}
-//		},		
-//		reflectX : function(position){
-//			return {
-//				x : position.x,
-//				y : -position.y
-//			}
-//		},		
-//		reflectY : function(position){
-//			return {
-//				x: -position.x,
-//				y: position.y
-//			}	
-//		},
-//		translate : function(position,x,y){
-//			return {
-//				x : position.x+x,
-//				y : position.y+y
-//			}
-//		}
-//}
-
-
-var BattleshipPosition = function(xCoords, yCoords, offsetCellX, offsetCellY){
+var BattleshipPosition = function(xCoords, yCoords){
 	
-	var transforms = [];
-	var cartesianCoords = [];
-	this.offsetCellX = offsetCellX;
-	this.offsetCellY = offsetCellY;
-	
-	var initialCoords = [];
-	for (var i=0;i<xCoords.length;i++){
-		initialCoords.push({
+	var coords = [];
+	for (var i = 0; i < xCoords.length; i++){
+		coords.push({
 			x:xCoords[i],
 			y:yCoords[i]
 		});
 	}
 	
-	var reset = function(){
-		transforms = [];
-		cartesianCoords = [];
-		for (var i=0;i<initialCoords.length;i++){
-			cartesianCoords.push({
-				x:initialCoords[i].x+0.5,
-				y:initialCoords[i].y+0.5
-			});
-		}
-	};
-	
-	//Compute the initial coords
-	reset();
-	
-	var reflectX = function(){
-		transforms.push(Transforms.reflectX);
-		for (var i=0;i<cartesianCoords.length;i++){
-			cartesianCoords[i] = Transforms.reflectX(cartesianCoords[i]);			
-		}
-	};
-	
-	var reflectY = function(){
-		transforms.push(Transforms.reflectY);
-		for (var i=0;i<cartesianCoords.length;i++){
-			cartesianCoords[i] = Transforms.reflectY(cartesianCoords[i]);			
-		}
-	};
-	
-	var rotateClockwise = function(){
-		transforms.push(Transforms.rotateClockwise);
-		for (var i=0;i<cartesianCoords.length;i++){
-			cartesianCoords[i] = Transforms.rotateClockwise(cartesianCoords[i]);			
-		}
-	};
-	
-	var rotateCounterClockwise = function(){
-		transforms.push(Transforms.rotateCounterClockwise);
-		for (var i=0;i<cartesianCoords.length;i++){
-			cartesianCoords[i] = Transforms.rotateCounterClockwise(cartesianCoords[i]);			
-		}
-	};
-	
-	var getCartesianCoords = function(){
-		var result = [];
-		for (var i=0;i<cartesianCoords.length;i++){
-			result.push(cartesianCoords[i]);
-		}
-		return result;
-	};
-	
 	var getCoords = function(){
-//		var result = [];
-//		for (var i=0;i<cartesianCoords.length;i++){
-//			result.push(Transforms.translate(cartesianCoords[i],-0.5,-0.5));
-//		}
-//		return result;
-		return initialCoords;
+		return coords;
 	};
 	
 	var getXCoords = function(){
 		var coords = getCoords();
 		var result = [];
-		for (var i=0;i<coords.length;i++){
+		for (var i = 0;i < coords.length;i++){
 			result.push(coords[i].x);
 		}
 		return result;
@@ -118,7 +24,7 @@ var BattleshipPosition = function(xCoords, yCoords, offsetCellX, offsetCellY){
 	var getYCoords = function(){
 		var coords = getCoords();
 		var result = [];
-		for (var i=0;i<coords.length;i++){
+		for (var i = 0;i < coords.length;i++){
 			result.push(coords[i].y);
 		}
 		return result;
@@ -126,17 +32,9 @@ var BattleshipPosition = function(xCoords, yCoords, offsetCellX, offsetCellY){
 	
 	
 	return {
-		reflectX : reflectX,
-		reflectY : reflectY,
-		rotateClockwise : rotateClockwise,
-		rotateCounterClockwise : rotateCounterClockwise,
-		getCartesianCoords : getCartesianCoords,
 		getCoords : getCoords,
 		getXCoords : getXCoords,
 		getYCoords : getYCoords,
-//		undo : undo,
-//		redo : redo,
-		reset : reset
 	};
 }
 
@@ -175,11 +73,6 @@ var gameEditing = function(data){
 	gameBoard.drawBoard();
 	transformationBoard.drawBoard();
 	selectionBoard.drawBoard();
- 		
- 	
-	$(document).on("selectedShipChanged",function(event,data){
-		$selectedShip = data;
-	});
 
  	/*	Draw ships in ships-container - June 25. 2015. added idIndex, so 
  	 * 	redraw function could be easier. Supposed BUGS could be available   */
@@ -204,32 +97,7 @@ var gameEditing = function(data){
  	var x4 = [0,1,2,3,4];
  	var y4 = [0,0,0,0,0];
  	selectionBoard.drawShip(x4, y4);
-
  	
- 	
- 	// Needs to be edited: store commands to vector(s).
-// 	var applyTransformation = function(transformationName){
-// 		var $ship = $('div.transformShip');
-// 		var position = $ship.data("position");
-// 		position[transformationName]();
-// 		var xCoords = $ship.data("position").getXCoords();
-// 		var yCoords = $ship.data("position").getYCoords();
-// 		$ship.remove();
-// 	 	board.drawShip(xCoords, yCoords, 6, 6, $transformationContainer);	
-// 	}
- 	
- 	
- 			//ROTATIONS
-// 	$('button#left').click(function(){ 		
-// 		applyTransformation("rotateCounterClockwise");
-// 	});
- 	
-// 	$('button#right').click(function(){
-// 		applyTransformation("rotateClockwise");
-// 	});
- 	
- 	///////////////////////////////////////////////////////////////////////////////////////////////////////////////
- 	//TODO:put droppable here in case you can't figure it out(note)
 }
  
 	// Stuff to do:
